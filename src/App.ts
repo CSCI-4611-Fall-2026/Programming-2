0882340867c738f80081d901b7b34005b0022c13
Evan Suma Rosenberg <suma@umn.edu>
2026-09-17 12:49:11 -0500

    pre-class commit

---
+++ src/App.ts
@@ -0,0 +1,31 @@
+/* Programming 2
+ * CSCI 4611, Fall 2026, University of Minnesota
+ * Instructor: Evan Suma Rosenberg <suma@umn.edu>
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ */ 
+
+import * as gfx from 'gophergfx'
+
+export class App extends gfx.GfxApp
+{
+    // --- Create the App class ---
+    constructor()
+    {
+        // Initialize the base class gfx.GfxApp
+        super();
+    }
+
+
+    // --- Initialize the graphics scene ---
+    createScene(): void 
+    {
+        
+    }
+
+    
+    // --- Update is called once each frame by the main graphics loop ---
+    update(deltaTime: number): void 
+    {
+        
+    }
+}
